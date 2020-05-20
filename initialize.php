d1d4de72e15c236000547dde8ed1c9cdd6d2f742
Leonardo Losoviz <leo@getpop.org>
2020-05-20 19:25:11 +0800

    Implemented abstract function for migrate packages

--- initialize.php
+++ initialize.php
@@ -1,6 +1,11 @@
 <?php
 $class = (new class() extends \PoP\Root\Component\AbstractComponent
 {
+    public static function getDependedComponentClasses(): array
+    {
+        return [];
+    }
+
     /**
      * Boot component
      *
