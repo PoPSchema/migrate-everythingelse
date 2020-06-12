62d588b81dee844f3b15044b290d830367c0e816
Leonardo Losoviz <leo@getpop.org>
2020-06-12 09:21:57 +0800

    Moved code for events and locations to own migrate repos

--- migrate/plugins/pop-events/library/functions/load.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once 'constants.php';
-require_once 'category.php';
-require_once 'functions.php';
-require_once 'url-params.php';
-require_once 'vars-hooks.php';
-require_once 'userpreferences-values.php';
-require_once 'pop-engine-utils.php';
--- migrate/plugins/pop-events/plugins/pop-coauthors/library/functions/functions.php
+++
@@ -1,4 +0,0 @@
-<?php
-use PoP\Hooks\Facades\HooksAPIFacade;
-
-HooksAPIFacade::getInstance()->addFilter('popcomponent:coauthors:supportedposttypes', 'gdEmAddEventPosttype');
