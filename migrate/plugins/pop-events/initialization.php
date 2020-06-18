ba20e835b2c82943e0645249c60ca0ca68cf79a7
Leonardo Losoviz <leo@getpop.org>
2020-06-18 19:14:57 +0800

    Moved from migrate-events to everythingelse

--- migrate/plugins/pop-events/initialization.php
+++ migrate/plugins/pop-events/initialization.php
@@ -4,8 +4,18 @@ class PoP_Events_Initialization
     public function initialize()
     {
         /**
+         * Load the Config
+         */
+        include_once 'config/load.php';
+
+        /**
          * Load the Library
          */
         include_once 'library/load.php';
+
+        /**
+         * Load the Plugins Library
+         */
+        include_once 'plugins/load.php';
     }
 }
--- migrate/plugins/pop-events/library/functions/functions.php
+++ migrate/plugins/pop-events/library/functions/functions.php
@@ -1,21 +1,62 @@
 <?php
+use PoP\Translation\Facades\TranslationAPIFacade;
 use PoP\Hooks\Facades\HooksAPIFacade;
+use PoP\ComponentModel\ModuleProcessors\Constants;
+use PoP\Events\Facades\EventTypeAPIFacade;
 
-/**
- * Needed to add the "All" category to all events, to list them for the Latest Everything Block
- */
-
-// Do always add the 'All' Category when adding a new event
-HooksAPIFacade::getInstance()->addAction('em_event_save_pre', 'gdEmEventSavePreAddAllCategory', 10, 1);
-function gdEmEventSavePreAddAllCategory($EM_Event)
+// HooksAPIFacade::getInstance()->addFilter('gd_dataload:post_types', 'gdEmAddEventPosttype');
+function gdEmAddEventPosttype($post_types)
 {
-
-    // Only do it if filtering by taxonomy is enabled. Otherwise no need
-    if (defined('POP_TAXONOMYQUERY_INITIALIZED') && PoP_Application_TaxonomyQuery_ConfigurationUtils::enableFilterAllcontentByTaxonomy()) {
-        if (defined('POP_EVENTS_CAT_ALL') && POP_EVENTS_CAT_ALL) {
-            if (!$EM_Event->get_categories()->terms[POP_EVENTS_CAT_ALL]) {
-                $EM_Event->get_categories()->terms[POP_EVENTS_CAT_ALL] = new EM_Category(POP_EVENTS_CAT_ALL);
-            }
-        }
-    }
+    $eventTypeAPI = EventTypeAPIFacade::getInstance();
+    $post_types[] = $eventTypeAPI->getEventCustomPostType();
+    return $post_types;
 }
+
+function eventHasCategory($event, $cat)
+{
+    $eventTypeAPI = EventTypeAPIFacade::getInstance();
+    $categories = $eventTypeAPI->getCategories($event);
+    return isset($categories[$cat]);
+}
+
+// HooksAPIFacade::getInstance()->addFilter('gdGetCategories', 'gdEmGetCategories', 10, 2);
+// function gdEmGetCategories($categories, $post_id)
+// {
+//     $eventTypeAPI = EventTypeAPIFacade::getInstance();
+//     if ($eventTypeAPI->isEvent($post_id)) {
+//         $event = $eventTypeAPI->getEvent($post_id);
+//         return array_keys($eventTypeAPI->getCategories($event));
+//     }
+
+//     return $categories;
+// }
+
+HooksAPIFacade::getInstance()->addFilter('gd_postname', 'gdEmPostnameImpl', 10, 2);
+function gdEmPostnameImpl($name, $post_id)
+{
+    $eventTypeAPI = EventTypeAPIFacade::getInstance();
+    if ($eventTypeAPI->isEvent($post_id)) {
+        return TranslationAPIFacade::getInstance()->__('Event', 'poptheme-wassup');
+    }
+
+    return $name;
+}
+HooksAPIFacade::getInstance()->addFilter('gd_posticon', 'gdEmPosticonImpl', 10, 2);
+function gdEmPosticonImpl($icon, $post_id)
+{
+    $eventTypeAPI = EventTypeAPIFacade::getInstance();
+    if ($eventTypeAPI->isEvent($post_id)) {
+        return getRouteIcon(POP_EVENTS_ROUTE_EVENTS, false);
+    }
+
+    return $icon;
+}
+
+HooksAPIFacade::getInstance()->addFilter(
+    Constants::HOOK_QUERYDATA_WHITELISTEDPARAMS,
+    function($params) {
+        $params[] = GD_URLPARAM_YEAR;
+        $params[] = GD_URLPARAM_MONTH;
+        return $params;
+    }
+);
