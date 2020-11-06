379f74cd3c8ea633631953c9d76e4d9e6d8c932c
Leonardo Losoviz <leo@getpop.org>
2020-11-06 12:40:06 +0800

    Made getSuccessString optional

--- migrate/plugins/pop-addhighlights/library/dataload/actionexecuters/highlight-createupdate.php
+++ migrate/plugins/pop-addhighlights/library/dataload/actionexecuters/highlight-createupdate.php
@@ -8,23 +8,23 @@ use PoPSchema\CustomPostMutations\MutationResolvers\AbstractCreateUpdateCustomPo
 
 abstract class GD_DataLoad_ActionExecuter_CreateUpdate_Highlight extends AbstractCreateUpdateCustomPostMutationResolverBridge
 {
-    public function getSuccessString($post_id): string
+    public function getSuccessString($result_id): ?string
     {
         $customPostTypeAPI = CustomPostTypeAPIFacade::getInstance();
-        $status = $customPostTypeAPI->getStatus($post_id);
+        $status = $customPostTypeAPI->getStatus($result_id);
         if ($status == Status::PUBLISHED) {
             // Give a link to the referenced post to the stance, and force it to get it from the server again
-            $highlighted = \PoPSchema\CustomPostMeta\Utils::getCustomPostMeta($post_id, GD_METAKEY_POST_HIGHLIGHTEDPOST, true);
+            $highlighted = \PoPSchema\CustomPostMeta\Utils::getCustomPostMeta($result_id, GD_METAKEY_POST_HIGHLIGHTEDPOST, true);
             $success_string = sprintf(
                 TranslationAPIFacade::getInstance()->__('<a href="%s" %s>Click here to view it</a>.', 'poptheme-wassup'),
                 $customPostTypeAPI->getPermalink($highlighted),
                 getReloadurlLinkattrs()
             );
 
-            return HooksAPIFacade::getInstance()->applyFilters('gd-createupdate-uniquereference:execute:successstring', $success_string, $post_id, $status);
+            return HooksAPIFacade::getInstance()->applyFilters('gd-createupdate-uniquereference:execute:successstring', $success_string, $result_id, $status);
         }
 
-        return parent::getSuccessString($post_id);
+        return parent::getSuccessString($result_id);
     }
 }
 
