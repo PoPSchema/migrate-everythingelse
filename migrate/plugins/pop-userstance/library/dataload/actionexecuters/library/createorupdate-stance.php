dd0c9f0a8233b8173cb5c98d86594ce2921682f0
Leonardo Losoviz <leo@getpop.org>
2020-11-16 18:31:51 +0800

    Moved validation upwards

--- migrate/plugins/pop-userstance/library/dataload/actionexecuters/library/createorupdate-stance.php
+++ migrate/plugins/pop-userstance/library/dataload/actionexecuters/library/createorupdate-stance.php
@@ -3,32 +3,6 @@
 
 class GD_CreateOrUpdate_Stance extends GD_CreateUpdate_Stance
 {
-    public function validateErrors(array $form_data): ?array
-    {
-        $errors = [];
-        // If there's post_id => It's Update
-        // Otherwise => It's Create
-        $post_id = $this->getUpdateCustomPostID();
-
-        if ($post_id) {
-            // If already exists any of these errors above, return errors
-            $this->validateUpdate($errors);
-            if ($errors) {
-                return $errors;
-            }
-            $this->validateUpdateContent($errors, $form_data);
-        } else {
-            // If already exists any of these errors above, return errors
-            $this->validateCreate($errors);
-            if ($errors) {
-                return $errors;
-            }
-            $this->validateCreateContent($errors, $form_data);
-        }
-        $this->validateContent($errors, $form_data);
-        return $errors;
-    }
-
     /**
      * @return mixed
      */
