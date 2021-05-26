8ee7fd0dfd62d09c866d7a9390e665fa6409d107
leoloso <leo@getpop.org>
2021-05-26 10:30:29 +0000

    Merge pull request #672 from leoloso/migrate-users-to-psr-4
    Migrate Users packages to PSR-4

---
+++ migrate/migrate-packages/migrate-users-wp/migrate/initialization.php
@@ -0,0 +1,15 @@
+<?php
+namespace PoPSchema\Users\WP;
+
+class Initialization
+{
+    public function initialize()
+    {
+        load_plugin_textdomain('pop-users-wp', false, dirname(plugin_basename(__FILE__)).'/languages');
+
+        /**
+         * Load the Contract Implementations
+         */
+        include_once 'contractimplementations/load.php';
+    }
+}
---
+++ migrate/migrate-packages/migrate-users/migrate/initialization.php
@@ -0,0 +1,15 @@
+<?php
+namespace PoPSchema\Users;
+
+class Initialization
+{
+    public function initialize()
+    {
+        load_plugin_textdomain('pop-users', false, dirname(plugin_basename(__FILE__)).'/languages');
+
+        /**
+         * Load the Contracts
+         */
+        include_once 'contracts/load.php';
+    }
+}
